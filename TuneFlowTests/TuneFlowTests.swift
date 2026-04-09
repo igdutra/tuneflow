c1e6ab27851163ba35abb31655a1a7a33770a2b7
Ivo Dutra <ivogdutra@gmail.com>
2026-04-09 18:08:17 -0300

    Connect local TuneAPI package with main project

--- TuneFlowTests/TuneFlowTests.swift
+++ TuneFlowTests/TuneFlowTests.swift
@@ -7,11 +7,18 @@
 
 import Testing
 @testable import TuneFlow
+import TuneAPI
+
+struct TuneAPIStub: TuneRepository {
+    func fetchTunes() async throws -> [String] {
+        ["This is a stub"]
+    }
+}
 
 struct TuneFlowTests {
 
     @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        let spy = TuneAPIStub()
+        try await #expect(spy.fetchTunes().first == "This is a stub")
     }
-
 }
