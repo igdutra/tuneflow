351f421ab8f9cdd3a50176f52f84d69eba5a669e
Ivo Dutra <ivogdutra@gmail.com>
2026-04-13 20:10:33 -0300

    Add concrete implementation of `LogHandling` using OSLog: free alternative and structured logs without 3rd party dependency

--- TuneFlow/TuneApp/LogHandling/OSLogger.swift
+++ TuneFlow/TuneApp/LogHandling/OSLogger.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import TuneDomain
+import OSLog
+
+struct OSLogger: LogHandling {
+    private let logger: Logger
+
+    public init(
+        subsystem: String = Bundle.main.bundleIdentifier ?? "com.ivo.tuneFlow",
+        category: String = "app"
+    ) {
+        self.logger = Logger(
+            subsystem: subsystem,
+            category: category
+        )
+    }
+
+    func error(_ message: String) {
+        logger.error("\(message, privacy: .public)")
+    }
+}
